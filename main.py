888f8268ab8e0cb58bec51de671b3c414e158199
alirezaeiji <alirezaeiji191379@gmail.com>
2025-01-26 10:57:43 +0330

    add switch for enc and dec

--- main.py
+++ main.py
@@ -9,16 +9,21 @@ x = ProjectBlockCipher()
 # print(cipher_text)
 # print(binary_to_text(cipher_text))
 # print("decrypted: "+ binary_to_text(plain_text))
-
+is_enc = True
+plain_text_path = "./plain_text.txt"
+cipher_text_path = "./result_cipher.txt"
+if not is_enc:
+    plain_text_path = "./result_cipher.txt"
+    cipher_text_path = "./plain_text.txt"
 with open("./key.txt", 'r', encoding='ascii') as file:
     key = file.read()
-with open("./plain_text.txt", 'r', encoding='ascii') as text_file:
+with open(plain_text_path, 'r', encoding='ascii') as text_file:
     plain_text = text_file.read()
 
-cipher_text = x.encrypt(plain_text, key, True)
+cipher_text = x.encrypt(plain_text, key, is_enc)
 decrypted = x.encrypt(cipher_text, key, False)
 print(binary_to_text(cipher_text))
 print(binary_to_text(decrypted))
-with open("./result_cipher.txt", 'w', encoding='ascii') as cipher_text_file:
+with open(cipher_text_path, 'w', encoding='ascii') as cipher_text_file:
     cipher_text_file.write(cipher_text)
 
