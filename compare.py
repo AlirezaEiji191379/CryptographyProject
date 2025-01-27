67ecbb77411158d4bdfe4ffa69636e07bb0f3fda
alirezaeiji <alirezaeiji191379@gmail.com>
2025-01-27 22:42:48 +0330

    compare des with crypto

--- compare.py
+++ compare.py
@@ -5,7 +5,7 @@ from Ciphers.ProjectBlockCipher import ProjectBlockCipher
 from Utilities.CipherUtilities import text_to_binary
 
 start_time = time.time()
-for _ in range(0, 1000):
+for _ in range(0, 10000):
     key0 = DesKey(b"some key")
     t = key0.encrypt(b"any long message")
 end_time = time.time()
@@ -15,10 +15,10 @@ encryption_time_avg = (end_time - start_time) / 10000
 print(f"Average encryption time: {encryption_time_avg:.10f} seconds")
 
 start_time = time.time()
-x = ProjectBlockCipher()
+x = ProjectBlockCipher(9, 6)
 key = text_to_binary("12345678909876543210")
 pt = text_to_binary("ahmadahmadahmadahmad")
-for _ in range(0, 1000):
+for _ in range(0, 10000):
    tt = x.encrypt(pt, key)
 end_time = time.time()
 
